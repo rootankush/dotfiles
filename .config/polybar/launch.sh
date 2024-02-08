0950fdc3e97a3f940484a090bf7147cf3f3cc4b1
rootankush <arun1972.pradhan@gmail.com>
2024-02-08 22:05:57 +0530

    tweaks

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,8 +7,7 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" | tee -a /tmp/polybar1.log 
 polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
-#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
