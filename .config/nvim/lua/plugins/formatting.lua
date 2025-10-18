e8bef5c6a6b714d8527e447d14abe3e324e59252
rootankush <rootankushprompt@gmail.com>
2025-10-18 19:22:08 +0530

    Removed nvim config

--- .config/nvim/lua/plugins/formatting.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'stevearc/conform.nvim',
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            javascript = { "prettier", stop_after_first = true },
-        },
-    },
-}
